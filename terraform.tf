1d3d20ab716a8df27ab89ace5511aaa34a42078f
Hidenori MATSUKI <MATSUKI.Hidenori@gmail.com>
2018-02-04 16:38:49 +0900

    Create CF for website, but cannot configure the CF :<.

--- terraform.tf
+++ terraform.tf
@@ -36,8 +36,8 @@ resource "aws_route53_zone" "sakemeshi-love" {
 # --------------------------------
 # S3 buckets: sakemeshi.love
 
-resource "aws_s3_bucket" "sakemeshi-love-website-prod" {
-  bucket = "sakemeshi-love-website-prod"
+resource "aws_s3_bucket" "sakemeshi-love-website-prod-s3" {
+  bucket = "sakemeshi-love-website-prod-s3"
   acl = "public-read"
   policy = <<POLICY
 {
@@ -54,8 +54,8 @@ resource "aws_s3_bucket" "sakemeshi-love-website-prod" {
         "s3:*"
       ],
       "Resource": [
-        "arn:aws:s3:::sakemeshi-love-website-prod",
-        "arn:aws:s3:::sakemeshi-love-website-prod/*"
+        "arn:aws:s3:::sakemeshi-love-website-prod-s3",
+        "arn:aws:s3:::sakemeshi-love-website-prod-s3/*"
       ]
     }
   ]
@@ -69,3 +69,60 @@ POLICY
     }
     force_destroy = true
 }
+
+# --------------------------------
+# CloudFront: sakemeshi.love
+
+#resource "aws_cloudfront_distribution" "sakemeshi-love-website-prod-distribution" {
+#  origin {
+#    domain_name = "${aws_s3_bucket.sakemeshi-love-website-prod-s3.bucket_domain_name}"
+#    origin_id = "sakemeshi-love-prod-origin"
+#    s3_origin_config {
+#      origin_access_identity = "origin-access-identity/cloudfront/${var.aws_account_id}"
+#    }
+#  }
+#
+#  enabled = true
+#  is_ipv6_enabled = true
+#  default_root_object = "index.html"
+#
+#  # ToDo
+#  #logging_config {
+#  #}
+#
+#  aliases = [
+#    "sakemeshi.love"
+#  ]
+#
+#  default_cache_behavior {
+#    allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+#    cached_methods = ["GET", "HEAD"]
+#    target_origin_id = "sakemeshi-love-prod-origin"
+#    forwarded_values {
+#      query_string = false
+#      cookies {
+#        forward = "none"
+#      }
+#    }
+#    viewer_protocol_policy = "allow-all"
+#    min_ttl = 0
+#    default_ttl = 3600
+#    max_ttl = 86400
+#  }
+#
+#  price_class = "PriceClass_200"
+#
+#  restrictions {
+#    geo_restriction {
+#      restriction_type = "blacklist"
+#      locations = []
+#    }
+#  }
+#
+#  tags {
+#  }
+#
+#  viewer_certificate {
+#    cloudfront_default_certificate = true
+#  }
+#}
