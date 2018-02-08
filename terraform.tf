4b4aa0443c83463ffdd805d3501d2f63ed2360d7
Hidenori MATSUKI <MATSUKI.Hidenori@gmail.com>
2018-02-08 21:28:27 +0900

    Setup CloudFront.

--- terraform.tf
+++ terraform.tf
@@ -45,6 +45,17 @@ resource "aws_s3_bucket" "sakemeshi-love-website-prod-s3" {
   "Statement": [
     {
       "Effect": "Allow",
+      "Principal": "*",
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Resource": [
+        "arn:aws:s3:::sakemeshi-love-website-prod-s3",
+        "arn:aws:s3:::sakemeshi-love-website-prod-s3/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
       "Principal": {
         "AWS": [
           "arn:aws:iam::${var.aws_account_id}:user/sakemeshi-love-website-prod-writer"
@@ -73,56 +84,59 @@ POLICY
 # --------------------------------
 # CloudFront: sakemeshi.love
 
-#resource "aws_cloudfront_distribution" "sakemeshi-love-website-prod-distribution" {
-#  origin {
-#    domain_name = "${aws_s3_bucket.sakemeshi-love-website-prod-s3.bucket_domain_name}"
-#    origin_id = "sakemeshi-love-prod-origin"
-#    s3_origin_config {
-#      origin_access_identity = "origin-access-identity/cloudfront/${var.aws_account_id}"
-#    }
-#  }
-#
-#  enabled = true
-#  is_ipv6_enabled = true
-#  default_root_object = "index.html"
-#
-#  # ToDo
-#  #logging_config {
-#  #}
-#
-#  aliases = [
-#    "sakemeshi.love"
-#  ]
-#
-#  default_cache_behavior {
-#    allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-#    cached_methods = ["GET", "HEAD"]
-#    target_origin_id = "sakemeshi-love-prod-origin"
-#    forwarded_values {
-#      query_string = false
-#      cookies {
-#        forward = "none"
-#      }
-#    }
-#    viewer_protocol_policy = "allow-all"
-#    min_ttl = 0
-#    default_ttl = 3600
-#    max_ttl = 86400
-#  }
-#
-#  price_class = "PriceClass_200"
-#
-#  restrictions {
-#    geo_restriction {
-#      restriction_type = "blacklist"
-#      locations = []
-#    }
-#  }
-#
-#  tags {
-#  }
-#
-#  viewer_certificate {
-#    cloudfront_default_certificate = true
-#  }
-#}
+resource "aws_cloudfront_origin_access_identity" "sakemeshi-love-website-prod-origin_access_identity" {
+}
+
+resource "aws_cloudfront_distribution" "sakemeshi-love-website-prod-distribution" {
+  origin {
+    #domain_name = "${aws_s3_bucket.sakemeshi-love-website-prod-s3.website_endpoint}"
+    domain_name = "${aws_s3_bucket.sakemeshi-love-website-prod-s3.bucket_domain_name}"
+    origin_id = "sakemeshi-love-prod-origin"
+    s3_origin_config {
+      origin_access_identity = "${aws_cloudfront_origin_access_identity.sakemeshi-love-website-prod-origin_access_identity.cloudfront_access_identity_path}"
+    }
+  }
+
+  enabled = true
+  is_ipv6_enabled = true
+  default_root_object = "index.html"
+
+  # ToDo
+  #logging_config {
+  #}
+
+  aliases = [
+    "sakemeshi.love"
+  ]
+
+  default_cache_behavior {
+    allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods = ["GET", "HEAD"]
+    target_origin_id = "sakemeshi-love-prod-origin"
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "allow-all"
+    min_ttl = 0
+    default_ttl = 3600
+    max_ttl = 86400
+  }
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags {
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
